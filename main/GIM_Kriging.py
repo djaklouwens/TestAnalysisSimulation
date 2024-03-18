9526d65d208583ee8fc8b35b2db7d8c1d6f1c0ca
Daniël Klouwens <dklouwens@tudelft.nl>
2024-03-18 12:12:05 +0100

    Rebuild Kriging to new coordinate system done

--- main/GIM_Kriging.py
+++ main/GIM_Kriging.py
@@ -15,91 +15,109 @@ ds = nc.Dataset(fn)
 tecmatrix =  ds['tecmap'][time,:]
 
 def tec(x, y):
-   # y = y % 180
+
     x = x % 360
-   #  if y == 180:
-   #     y = 0
+
     if x == 360:
         x = 0
     return tecmatrix[y, x]
 
 def geo_to_index(lon, lat, rounding=False):
-    # index [0,0] is the top left corner of the grid, corresponding to geo [181.5, 89.5]
+    # index [0,0] is the top left corner of the grid, corresponding to geo [180.5, 89.5]
     # convert from geocoordinates to indexcoordinates
-    if rounding:                                                       
-        x = round(lon - 181.5)
-        y = round(abs(lat - 89.5))
-        return x,y
-    if not rounding:
-        x = lon - 181.5
-        y = abs(lat - 89.5)
-        return x,y
+    if lon >= 180.5 and lon <= 360:
+        x = lon - 180.5
     
+    elif lon >= 0 and lon < 180.5:
+        x = lon + 179.5
+        
+    else:
+        print("Error: longitude (", lon, ") out of range")
+        return
+    
+    if lat >= -90 and lat <= 90:
+        y = abs(lat - 89.5)
+    else:
+        print("Error: latitude (", lat, ") out of range")
+        return
+       
+    if rounding:                                                       
+        x = int(x)
+        y = int(y)
+    
+    return x,y
+
 def index_to_geo(x, y): 
-    # index [0,0] is the top left corner of the grid, corresponding to geo [-179.5, 89.5] and sphere [180.5, 89.5]
+    # index [0,0] is the top left corner of the grid, corresponding to geo [180.5, 89.5]
     # convert from indexcoordinates to geocoordinates
-    lon = x - 179.5 #implement wrap around
-    lat = 89.5 - y
+    lon = np.array([])
+    lat = np.array([])
+    
+    for xi in x:
+        if xi >= -180 and xi <= 179.5:
+            lon = np.append(lon, xi + 180.5)
+        elif xi > 179.5 and xi <= 539:
+            lon = np.append(lon, xi - 179.5)
+
+        else:
+            print("Error: x (", xi, ") out of range")
+            return
+        
+    for yi in y:
+        if yi >= 0 and yi <= 180:
+            lat = np.append(lat, -yi + 89.5)
+        else:
+            print("Error: y (", yi, ") out of range")
+            return
     return lon, lat
 
 
-def tec_kriging(lon, lat, lon_halfrange=45, lat_halfrange=22, image=False):   
+
+def tec_kriging(lon, lat, lon_halfrange=45, lat_halfrange=22, image=False, plot_variogram=False):   
     x,y = geo_to_index(lon, lat, rounding=True)
     x_array = np.linspace(x-lon_halfrange, x+lon_halfrange, 2*lon_halfrange+1).astype(int)
     y_array = np.linspace(y-lat_halfrange, y+lat_halfrange, 2*lat_halfrange+1).astype(int)
-    #mask = y_array >= 0 | y_array <= 159
-    #y_array = y_array[mask]
+    
     y_array = y_array[(159 >= y_array)]
     y_array = y_array[(y_array > 0)]
-    print(y_array)
-    print(y_array.max(), y_array.min())
+    
     z_array = np.array([])
     
     for i in range(len(x_array)):
         for j in range(len(y_array)):           
             z_array = np.append(z_array, tec(x_array[i], y_array[j]))
     
+    print(x_array)
     lon_array, lat_array = index_to_geo(x_array, y_array)        
     
     lon_dims = np.repeat(lon_array, len(lat_array))
     lat_dims = np.tile(lat_array, len(lon_array))
-
-    # x_dims = np.repeat(x_array, len(y_array))
-    # y_dims = np.tile(y_array, len(x_array))
     
-    # if(x_dims.size != y_dims.size or x_dims.size != z_array.size or y_dims.size != z_array.size):
-    #     print("Error: Array sizes do not match")
-    #     print("x_dims: ", x_dims.size, "y_dims: ", y_dims.size, "z_array: ", z_array.size)
-    #     return
+    if(lon_dims.size != lat_dims.size or lon_dims.size != z_array.size or lat_dims.size != z_array.size):
+        print("error: array sizes do not match")
+        print("lon_dims: ", lon_dims.size, "lat_dims: ", lat_dims.size, "z_array: ", z_array.size)
+        return
+
+    print(lon_array)
     OK = OrdinaryKriging(
         lon_dims,
         lat_dims,
         z_array,
-        variogram_model="gaussian",
+        variogram_model="spherical",
         verbose=False,
-        enable_plotting=True,
-        nlags=10,
+        enable_plotting=plot_variogram,
+        nlags=75,
         coordinates_type="geographic",
     )
 
-    # OK = OrdinaryKriging(
-    #     x_dims,
-    #     y_dims,
-    #     z_array,
-    #     variogram_model="gaussian",
-    #     verbose=False,
-    #     enable_plotting=True,
-    #     nlags=10,
-    #     coordinates_type="geographic",
-    # )
-    # if image:
-    #     z_results, ss_results = OK.execute("grid", x_array, y_array)
-    #     plt.imshow(z_results, extent=[lon-lon_halfrange, lon+lon_halfrange, lat+lat_halfrange, lat-lat_halfrange], origin="upper") #the extent is not correct yet
-    #     plt.colorbar()
-    #     plt.show()
+    if image:
+        z_results, ss_results = OK.execute("grid", lon_array+0.5, lat_array+0.5)
+        plt.imshow(z_results, extent=[min(lon_array), max(lon_array), min(lat_array), max(lat_array)] , origin="upper") #the extent is not correct yet
+        plt.colorbar()
+        plt.show()
+        print(z_results)
+    
     z1, ss1 = OK.execute("points", [lat], [lon])
     return z1
-    # x,y = geo_to_index(lon, lat)
-    # z1, ss1 = OK.execute("points", [x], [y])
-    # return z1
-print(tec_kriging(0,0, lon_halfrange=45, lat_halfrange=22, image=False))
+
+print(tec_kriging(180,0, lon_halfrange=45, lat_halfrange=22, image=True, plot_variogram=True))
