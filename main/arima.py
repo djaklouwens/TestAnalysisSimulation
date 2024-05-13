e338b045424c17348ae68864abceacb6f46fe2bd
CarlijnWeijst <165258869+CarlijnWeijst@users.noreply.github.com>
2024-05-13 09:34:21 +0200

    Update arima.py

--- main/arima.py
+++ main/arima.py
@@ -9,12 +9,9 @@ from sklearn.metrics import mean_squared_error
 import os
 from netCDF4 import Dataset
 
-nc = os.path.dirname(os.path.realpath('j3_no_iono_2021_30s.nc'))
-
 def smoothing(nc_file, arima_order=(1, 1, 1), simpl_exp_params={'smoothing_level': [0.2, 0.3, 0.5]}, holts_lin_params={'smoothing_level': [0.2, 0.3, 0.5], 'smoothing_trend': [0.05, 0.1, 0.2]}):
     # Load NetCDF data
-
-    nc_data = Dataset(nc, 'r')
+    nc_data = Dataset(nc_file, 'r')
 
     # Read variables from the NetCDF file
     time = nc_data.variables['time'][:]
@@ -33,26 +30,39 @@ def smoothing(nc_file, arima_order=(1, 1, 1), simpl_exp_params={'smoothing_level
     # Convert 'Date Time' column to datetime format
     df['Time'] = pd.to_datetime(df['Time'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
 
-    # Assuming 'dict_keys' is defined somewhere in your code
+    def ARIMA_model(df, p=1, q=1, d=1):
+        model = ARIMA(df, order=(p, d, q))  
+        results_ARIMA = model.fit(disp=-1)
+        rmse = np.sqrt(mean_squared_error(df[1:], results_ARIMA.fittedvalues))
+        return results_ARIMA, rmse
+
+    def exponential_smoothing(df, smoothing_level=0.2):
+        model = SimpleExpSmoothing(np.asarray(df))
+        model._index = pd.to_datetime(df.index)
+        fit = model.fit(smoothing_level=smoothing_level, optimized=False)
+        return fit
+
+    def holts_linear_smoothing(df, smoothing_level=0.3, smoothing_slope=0.05):
+        model = Holt(np.asarray(df))
+        model._index = pd.to_datetime(df.index)
+        fit = model.fit(smoothing_level=smoothing_level, smoothing_trend=smoothing_slope, optimized=False)
+        return fit
+
+    def get_best_model(rmse):
+        if rmse:
+            min_rmse = min(rmse.values())
+            best = [key for key in rmse if rmse[key] == min_rmse]
+            return best[0]
+        else:
+            return None
+
     if 'Time' in df.columns:
-        # Extract the series and time index
         time = df['Time'][::100]
-        # Access the column 'Date Time' and convert it to datetime
         df['Time'] = pd.to_datetime(df['Time'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
-
-        # For remaining unparsed datetime strings, try another format
-        # Specify the start and end dates for the interval you want to analyze
-
-        # Filter the DataFrame based on the specified interval
-
-        # Split the data into train and test sets
-
         if 'sla' in df.columns:
-            series = df['sla'][::100]  # Create a copy of the series
-            # Now you can define 'train' and 'test' using 'series'
-            train = series[:-int(len(series)/10)] # Create a copy of the train set
-            test = series[-int(len(series)/10):]  # Create a copy of the test set
-            # Define and train the ARIMA model
+            series = df['sla'][::100]
+            train = series[:-int(len(series) / 10)]
+            test = series[-int(len(series) / 10):]
             model = pm.auto_arima(train.values,
                                   start_p=1,
                                   start_q=1,
@@ -68,7 +78,6 @@ def smoothing(nc_file, arima_order=(1, 1, 1), simpl_exp_params={'smoothing_level
                                   suppress_warnings=True,
                                   stepwise=True)
             print(model.summary())
-            # Save the model using pickle
             with open('seasonal_o.pkl', 'wb') as f:
                 pickle.dump(model, f)
             all_vals = model.predict(n_periods=len(series), return_conf_int=False)
@@ -80,8 +89,7 @@ def smoothing(nc_file, arima_order=(1, 1, 1), simpl_exp_params={'smoothing_level
             plt.ylabel('Temp (Celcius)')
             plt.legend(['True', 'Predicted'])
             plt.show()
-            # Place your code here for making predictions and evaluation
-            # predictions for the test set with confidence values
+
             preds, conf_vals = model.predict(n_periods=len(test), return_conf_int=True)
             preds = pd.Series(preds, index=test.index)
 
@@ -90,9 +98,9 @@ def smoothing(nc_file, arima_order=(1, 1, 1), simpl_exp_params={'smoothing_level
 
             plt.plot(series)
             plt.plot(preds, color='darkgreen')
-            plt.fill_between(lower_bounds.index, 
-                             lower_bounds, 
-                             upper_bounds, 
+            plt.fill_between(lower_bounds.index,
+                             lower_bounds,
+                             upper_bounds,
                              color='k', alpha=.15)
 
             plt.title("Forecast for test values")
@@ -101,13 +109,11 @@ def smoothing(nc_file, arima_order=(1, 1, 1), simpl_exp_params={'smoothing_level
             plt.legend(['True', 'Predicted'])
             plt.show()
 
-            # After making predictions for simpl_exp and holts_lin
-            simpl_exp_preds = {}  # Dictionary to store predictions for simple exponential smoothing
-            holts_lin_preds = {}  # Dictionary to store predictions for Holt's linear smoothing
-            simpl_exp_rmse = {}   # Dictionary to store RMSE values for simple exponential smoothing
-            holts_lin_rmse = {}   # Dictionary to store RMSE values for Holt's linear smoothing
+            simpl_exp_preds = {}
+            holts_lin_preds = {}
+            simpl_exp_rmse = {}
+            holts_lin_rmse = {}
 
-            # Calculate predictions and RMSE values for simpl_exp
             for s in simpl_exp_params['smoothing_level']:
                 model = exponential_smoothing(train, smoothing_level=s)
                 preds = model.predict(start=1, end=len(test))
@@ -117,7 +123,6 @@ def smoothing(nc_file, arima_order=(1, 1, 1), simpl_exp_params={'smoothing_level
                 rmse_s = np.sqrt(mean_squared_error(test, preds))
                 simpl_exp_rmse[s] = rmse_s
 
-            # Calculate predictions and RMSE values for holts_lin
             for sl in holts_lin_params['smoothing_level']:
                 for ss in holts_lin_params['smoothing_trend']:
                     model = holts_linear_smoothing(train, smoothing_level=sl, smoothing_slope=ss)
@@ -128,21 +133,53 @@ def smoothing(nc_file, arima_order=(1, 1, 1), simpl_exp_params={'smoothing_level
                     rmse_hl = np.sqrt(mean_squared_error(test, preds))
                     holts_lin_rmse[(sl, ss)] = rmse_hl
 
-            # Now, you can find the best models
             best1 = get_best_model(simpl_exp_rmse)
             best2 = get_best_model(holts_lin_rmse)
 
-            # Plotting
-            plt.figure(figsize=(12,6))
+            plt.figure(figsize=(12, 6))
             plt.plot(train.index, train.values, color='gray')
             plt.plot(test.index, test.values, color='gray')
             plt.title('Simple and Holt Smoothing Forecast')
 
             preds = simpl_exp_preds[best1]
             rmse = np.sqrt(mean_squared_error(test, preds))
-            plt.plot(test.index[:len(preds)], preds, color='red', label='preds - simple exponential smoothing - RMSE - {}'.format(rmse))
+            plt.plot(test.index[:len(preds)], preds, color='red',
+                     label='preds - simple exponential smoothing - RMSE - {}'.format(rmse))
             plt.legend()
 
             preds = holts_lin_preds[best2]
             rmse = np.sqrt(mean_squared_error(test, preds))
-            plt.plot(test.index[:len(preds)], preds, color='green', label='preds - holts linear smoothing
+            plt.plot(test.index[:len(preds)], preds, color='green',
+                     label='preds - holts linear smoothing - RMSE - {}'.format(rmse))
+            plt.legend()
+
+            plt.show()
+
+            plt.figure(figsize=(12, 6))
+            plt.plot(train.index, train.values, color='gray')
+            plt.plot(test.index, test.values, color='gray')
+            plt.title('Best Model Forecast ')
+
+            preds = simpl_exp_preds[best1]
+            rmse = np.sqrt(mean_squared_error(test, preds))
+            plt.plot(test.index[:len(preds)], preds, color='red',
+                     label='preds - simple exponential smoothing - RMSE - {}'.format(rmse))
+            plt.legend()
+            plt.show()
+
+            plt.figure(figsize=(12, 6))
+            plt.plot(train.index, train.values, color='gray')
+            plt.plot(test.index, test.values, color='gray')
+            plt.title('Best Model Forecast ')
+
+            preds = holts_lin_preds[best2]
+            rmse = np.sqrt(mean_squared_error(test, preds))
+            plt.plot(test.index[:len(preds)], preds, color='green',
+                     label='preds - holts linear smoothing - RMSE - {}'.format(rmse))
+            plt.legend()
+            plt.show()
+        else:
+            print("Column 'sla' does not exist in the DataFrame.")
+    else:
+        print("Column 'Time' does not exist in the DataFrame.")
+
